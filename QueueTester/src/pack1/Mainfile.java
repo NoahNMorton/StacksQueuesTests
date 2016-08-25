a649e312945473b12d1ce2ce854740516db58b5b
Noah Morton <K1211282@students.katyisd.org>
2016-08-25 14:01:45 -0500

    Begun work on queuetester
    Created all files

---
+++ QueueTester/src/pack1/Mainfile.java
@@ -0,0 +1,13 @@
+package pack1;
+
+/**
+ * Author: Noah Morton
+ * Date created: 8/25/2016
+ * Part of project: QueueTester
+ **/
+public class Mainfile {
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ QueueTester/src/pack1/QueueInterface.java
@@ -0,0 +1,16 @@
+public interface QueueInterface<E>
+{
+	boolean add(E o);
+	boolean offer(E o);
+
+	E peek();
+	E element();
+
+	E poll();
+	E remove();
+
+	boolean empty();
+
+	int size();
+	E get(int x);
+}
